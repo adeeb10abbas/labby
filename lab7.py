2930a318f08fa709b2ad44a016175a6eb83ae07a
Adeeb Abbas <adeebabbas@outlook.com>
2019-05-17 08:56:45 -0400

    Add files via upload

--- lab7.py
+++ lab7.py
@@ -17,7 +17,6 @@ my_rooms.append(Room("This is a wardrobe of ties"))
 my_rooms.append(Room("This is a library"))
 my_rooms.append(Room("This is the exit. Hooray! Lucky you"))
 
-
 my_rooms[0].setNorth(my_rooms[1])
 my_rooms[1].setEast(my_rooms[2])
 my_rooms[1].setSouth(my_rooms[0])
@@ -40,6 +39,7 @@ my_rooms[9].setWest(my_rooms[8])
 my_rooms[10].setEast(my_rooms[11])
 my_rooms[10].setNorth(my_rooms[9])
 my_rooms[11].setWest(my_rooms[10])
+
 # Making the maze
 # Setting the start and exit room
 my_maze = Maze(my_rooms[0], my_rooms[11])
@@ -48,20 +48,38 @@ my_maze = Maze(my_rooms[0], my_rooms[11])
 
 def main():
     while True:
-        print(my_maze.getCurrent())
+        #print(my_maze.getCurrent())
         inp = input("Where would you like to go? north south east west reset\n")
         if inp.lower() == 'north':
             if my_maze.moveNorth() is True:
+                my_maze.moveNorth()
                 print(my_maze.getCurrent())
+            else:
+                print("Invalid Move")
 
         elif inp.lower() == 'south':
             if my_maze.moveSouth() is True:
+                my_maze.moveSouth()
                 print(my_maze.getCurrent())
+            else:
+                print("Invalid Move")
 
         elif inp.lower() == 'east':
+
             if my_maze.moveEast() is True:
+                my_maze.moveEast()
                 print(my_maze.getCurrent())
+            else:
+                print("Invalid Move")
         elif inp.lower() == 'west':
+
             if my_maze.moveWest() is True:
+                my_maze.moveWest()
                 print(my_maze.getCurrent())
+            else:
+                print("Invalid Move")
+
+
+
+
 main()
